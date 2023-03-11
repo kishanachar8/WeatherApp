bcb86d9fba7b82c3b9047eed2cf34af9a0d619c7
kishan achar <kishanachar8@gmail.com>
2023-03-11 10:30:34 +0530

    initial commit

---
+++ weather.js
@@ -0,0 +1,53 @@
+
+let city=document.getElementById('city')
+let country=document.getElementById('country')
+let teperature=document.getElementById('temperature')
+
+
+let user_input=document.getElementById('user_input')
+let checkTemp=async()=>{
+    let cityValue=user_input.value
+    if(cityValue==""){
+        alert('please enter city name before search')
+    }
+    else{
+        let response= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityValue}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`)
+          let data=await response.json()
+          city.textContent=data.name
+          country.textContent=data.sys.country
+          temperature.textContent=data.main.temp
+       
+          
+     
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// http://api.openweathermap.org/data/2.5/weather?q=bangalore&units=metric&appid=1cb6532aea3c298a830a71380eace21e
